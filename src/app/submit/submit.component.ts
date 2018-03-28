a310b48cb819fd69b3ee70a2841288d22513fa24
Aaron Suarez <aaron.j.suarez1@gmail.com>
2018-03-27 22:38:04 -0500

    Test something else out for deployment

--- src/app/submit/submit.component.ts
+++ src/app/submit/submit.component.ts
@@ -4,6 +4,7 @@ import { LocationService } from '../location.service';
 import { ZomatoService } from '../zomato.service';
 import { LocationData } from '../locationdata';
 import { RestaurantComponent } from '../restaurant/restaurant.component';
+import { environment } from '../../environments/environment.prod';
 declare var System: any;
 
 @Component({
@@ -25,34 +26,34 @@ export class SubmitComponent implements OnInit {
   }
 
   onSubmit() {
-    // this.numClicks++;
-    // if (this.numClicks > 6)
-    // {
-    //   alert("You can only view up to 6 random restaurants");
-    //   return;
-    // }
+    this.numClicks++;
+    if (this.numClicks > 6)
+    {
+      alert("You can only view up to 6 random restaurants");
+      return;
+    }
+
+    const zipcode = ((document.getElementById("zipcode") as HTMLInputElement).value);
+    const zip = parseInt(zipcode);
+    const dist = ((document.getElementById("distance") as HTMLInputElement).value);
+    const distance = parseInt(dist);
+
+    const zKey = environment.zomatoKey;
+    const gKey = environment.googleKey;
+
+    console.log(zKey)
+    console.log(gKey)
+
+    // this.locationService.getLocationData(gKey, zip)
+    // .subscribe((data: LocationData) => {
+    //   const lat = data.results[0].geometry.location.lat;
+    //   const lng = data.results[0].geometry.location.lng;
+    //   const radius = distance*1609.344; // convert miles to meters
     //
-    // let zKey;
-    // let gKey;
-    //
-    // const zip = (<HTMLInputElement>document.getElementById("zipcode")).value;
-    // const distance = <Number>(<HTMLInputElement>document.getElementById("distance")).value;
-    //
-    // import('../../assets/apikeys.json').then((file)=> {
-    //   zKey = file['zomato-key'];
-    //   gKey = file['google-key'];
-    //
-    //   this.locationService.getLocationData(gKey, zip)
-    //   .subscribe((data: LocationData) => {
-    //     const lat = data.results[0].geometry.location.lat;
-    //     const lng = data.results[0].geometry.location.lng;
-    //     const radius = distance*1609.344; // convert miles to meters
-    //
-    //     this.zomatoService.getRestaurantData(zKey, lat, lng, radius)
-    //     .subscribe(data => {
-    //       const restaurant = data.restaurants[Math.floor(Math.random()*data.restaurants.length)];
-    //       this.restaurant.setChoice(restaurant);
-    //     })
+    //   this.zomatoService.getRestaurantData(zKey, lat, lng, radius)
+    //   .subscribe(data => {
+    //     const restaurant = data.restaurants[Math.floor(Math.random()*data.restaurants.length)];
+    //     this.restaurant.setChoice(restaurant);
     //   })
     // })
   }
